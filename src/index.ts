12ddc470d668eff747be5a133ea802ba4311a7f6
Seba Di Lauro <jsdilauro@gmail.com>
2023-12-15 09:27:57 -0300

    add basic test for ui-text

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import { createBlackRoom } from './utils/black-room'
 
-// // This test should always be first
-// import './tests/transform/index.test'
+// This test should always be first
+import './tests/transform/index.test'
 
-// // TODO: it needs input simulation to test it
-// // import './tests/pointer-lock/index.test'
+// TODO: it needs input simulation to test it
+// import './tests/pointer-lock/index.test'
 
-// import './tests/raycast/index.test'
-// import './tests/camera-mode/index.test'
-// import './tests/billboard/index.test'
-// import './tests/engine-info/index.test'
+import './tests/raycast/index.test'
+import './tests/camera-mode/index.test'
+import './tests/billboard/index.test'
+import './tests/engine-info/index.test'
 
-// // snapshot tests:
-// import './tests/mesh-renderer/index.test'
-// import './tests/material/index.test'
-// import './tests/visibility/index.test'
-// import './tests/gltf-container/index.test'
-// import './tests/video-player/index.test'
-// import './tests/text-shape/index.test'
-// import './tests/avatar-attach/index.test'
+// snapshot tests:
+import './tests/mesh-renderer/index.test'
+import './tests/material/index.test'
+import './tests/visibility/index.test'
+import './tests/gltf-container/index.test'
+import './tests/video-player/index.test'
+import './tests/text-shape/index.test'
+import './tests/avatar-attach/index.test'
 import './tests/ui-background/index.test'
 import './tests/ui-text/index.test'
 
--- src/tests/ui-text/index.test.tsx
+++ src/tests/ui-text/index.test.tsx
@@ -29,15 +29,14 @@ function TestElementText() {
       uiBackground={{
         color: Color4.Red()
       }}
-    >
-      <Label
-        value="This is a label"
-        color={Color4.White()}
-        fontSize={80}
-        font="serif"
-        textAlign="middle-center"
-      />
-    </UiEntity>
+      uiText={{
+        value: 'White Text',
+        color: Color4.White(),
+        fontSize: 80,
+        font: 'serif',
+        textAlign: 'middle-center'
+      }}
+    ></UiEntity>
   )
 }
 
@@ -53,19 +52,64 @@ function TestElementTextBlue() {
       uiBackground={{
         color: Color4.Red()
       }}
-    >
-      <Label
-        value="This is a label"
-        color={Color4.Blue()}
-        fontSize={80}
-        font="serif"
-        textAlign="middle-center"
-      />
-    </UiEntity>
+      uiText={{
+        value: 'Blue Text',
+        color: Color4.Blue(),
+        fontSize: 80,
+        font: 'serif',
+        textAlign: 'middle-center'
+      }}
+    ></UiEntity>
   )
 }
 
-test('ui-text: should render the entire screen red with white label', async function (context) {
+function TestElementTextWhiteSansSerif() {
+  const screenSize = getScreenCanvasInfo()
+  // console.log("asd")
+  return (
+    <UiEntity
+      uiTransform={{
+        width: screenSize.w,
+        height: screenSize.h
+      }}
+      uiBackground={{
+        color: Color4.Red()
+      }}
+      uiText={{
+        value: 'Sans-Serif',
+        color: Color4.White(),
+        fontSize: 80,
+        font: 'sans-serif',
+        textAlign: 'middle-center'
+      }}
+    ></UiEntity>
+  )
+}
+
+function TestElementTextWhiteMonospace() {
+  const screenSize = getScreenCanvasInfo()
+  // console.log("asd")
+  return (
+    <UiEntity
+      uiTransform={{
+        width: screenSize.w,
+        height: screenSize.h
+      }}
+      uiBackground={{
+        color: Color4.Red()
+      }}
+      uiText={{
+        value: 'Monospace',
+        color: Color4.White(),
+        fontSize: 80,
+        font: 'monospace',
+        textAlign: 'middle-center'
+      }}
+    ></UiEntity>
+  )
+}
+
+test('ui-text: should render the entire screen red with white text', async function (context) {
   CustomReactEcsRenderer.destroy()
   CustomReactEcsRenderer.setUiRenderer(TestElementText)
   await context.helpers.waitNTicks(10)
@@ -82,6 +126,28 @@ test('ui-text: should render the entire screen red with blue label', async funct
   await context.helpers.waitNTicks(10)
   await assertSnapshot(
     'screenshot/$explorer_snapshot_ui_text_2.png',
+    Vector3.create(8, 1, 10),
+    Vector3.create(8, 1, 8)
+  )
+})
+
+test('ui-text: should render the entire screen red with white text sans serif', async function (context) {
+  CustomReactEcsRenderer.destroy()
+  CustomReactEcsRenderer.setUiRenderer(TestElementTextWhiteSansSerif)
+  await context.helpers.waitNTicks(10)
+  await assertSnapshot(
+    'screenshot/$explorer_snapshot_ui_text_3.png',
+    Vector3.create(8, 1, 10),
+    Vector3.create(8, 1, 8)
+  )
+})
+
+test('ui-text: should render the entire screen red with white text monospace', async function (context) {
+  CustomReactEcsRenderer.destroy()
+  CustomReactEcsRenderer.setUiRenderer(TestElementTextWhiteMonospace)
+  await context.helpers.waitNTicks(10)
+  await assertSnapshot(
+    'screenshot/$explorer_snapshot_ui_text_4.png',
     Vector3.create(8, 1, 10),
     Vector3.create(8, 1, 8)
   )
