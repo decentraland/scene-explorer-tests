0f352757161fa23c9cc5431387eef2c69eff5798
Seba Di Lauro <jsdilauro@gmail.com>
2023-12-21 13:23:11 -0300

    fix

--- 52,-56-billboard/src/index.ts
+++ 52,-56-billboard/src/index.ts
@@ -1,7 +1,7 @@
 import { createBlackRoom } from './utils/black-room'
 
 // This test should always be first
-import './tests/transform/index.test'
+import './tests/billboard/index.test'
 
 export function main(): void {
   createBlackRoom()
