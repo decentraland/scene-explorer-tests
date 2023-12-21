006ac5977ebfe66d4becc3f2a0c2d138d557643f
Lean Mendoza <leandro@decentraland.org>
2023-12-21 15:01:59 -0300

    wip clean up

--- 54,-52-material/src/index.ts
+++ 54,-52-material/src/index.ts
@@ -2,5 +2,10 @@ import { createBlackRoom } from 'testing-library/src/utils/black-room'
 import './tests/material/index.test'
 
 export function main(): void {
-  createBlackRoom()
+  createBlackRoom(
+
+    
+  )
 }
+
+
