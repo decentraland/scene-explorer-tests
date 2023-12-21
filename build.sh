9999a1af3eddfe759e0e6a0f6a8aefb562303459
Lean Mendoza <leandro@decentraland.org>
2023-12-21 15:02:41 -0300

    wip
    Signed-off-by: Lean Mendoza <leandro@decentraland.org>

--- build.sh
+++ build.sh
@@ -12,23 +12,23 @@ npm i $SDK_PACKAGE $JS_RUNTIME_PACKAGE --legacy-peer-deps
 # then the rest of the dependencies
 npm install --legacy-peer-deps
 
-# npm run sync
+npm run sync
 
-# npm ls @dcl/sdk
+npm ls @dcl/sdk
 
-# # clean the git state of package.json(s)
-# git add */package.json package.json
+# clean the git state of package.json(s)
+git add */package.json package.json
 
-# # and fail if git state is dirty
-# git diff --ignore-cr-at-eol --exit-code .
+# and fail if git state is dirty
+git diff --ignore-cr-at-eol --exit-code .
 
-# if [[ $? -eq 1 ]]; then
-#   echo "GIT IS ON DIRTY STATE 🔴 Please run 'npm run update-parcels' locally and commit"
-#   exit 1
-# fi
+if [[ $? -eq 1 ]]; then
+  echo "GIT IS ON DIRTY STATE 🔴 Please run 'npm run update-parcels' locally and commit"
+  exit 1
+fi
 
-# # ensure all packages are on sync
-# npm run sync && npm run test-sync
+# ensure all packages are on sync
+npm run sync && npm run test-sync
 
 # and lastly build scenes
 npm run build
