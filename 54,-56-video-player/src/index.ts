5dc0ca21fed13e2d7062b23dceab06c94c619531
Lean Mendoza <leandro@decentraland.org>
2023-12-26 20:54:43 -0300

    wip

--- 54,-56-video-player/src/index.ts
+++ 54,-56-video-player/src/index.ts
@@ -2,5 +2,5 @@ import { createBlackRoom } from 'testing-library/src/utils/black-room'
 import './tests/video-player/index.test'
 
 export function main(): void {
-  createBlackRoom()
+  // createBlackRoom()
 }
--- 54,-56-video-player/src/tests/video-player/index.test.ts
+++ 54,-56-video-player/src/tests/video-player/index.test.ts
@@ -3,7 +3,8 @@ import {
   Material,
   MeshRenderer,
   Transform,
-  VideoPlayer
+  VideoPlayer,
+  videoEventsSystem
 } from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
 
@@ -14,15 +15,15 @@ import { customAddEntity } from 'testing-library/src/utils/entity'
 
 test('video-player: if exist a reference snapshot should match with it', async function (context) {
   customAddEntity.clean()
-  const screen = customAddEntity.addEntity()
-  MeshRenderer.createOrReplace(screen, {
+  const screenEntity = customAddEntity.addEntity()
+  MeshRenderer.createOrReplace(screenEntity, {
     mesh: { $case: 'plane', plane: { uvs: [] } }
   })
-  Transform.create(screen, {
+  Transform.create(screenEntity, {
     position: Vector3.create(8, 8, 15),
     scale: Vector3.create(16, 16, 1)
   })
-  VideoPlayer.create(screen, {
+  VideoPlayer.create(screenEntity, {
     src: 'src/assets/videos/dae-video-1.mp4',
     playing: true,
     volume: 0.2,
@@ -30,9 +31,9 @@ test('video-player: if exist a reference snapshot should match with it', async f
     position: 0
   })
 
-  const videoTexture = Material.Texture.Video({ videoPlayerEntity: screen })
+  const videoTexture = Material.Texture.Video({ videoPlayerEntity: screenEntity })
 
-  Material.setPbrMaterial(screen, {
+  Material.setPbrMaterial(screenEntity, {
     texture: videoTexture,
     roughness: 1.0,
     specularIntensity: 0,
@@ -50,15 +51,26 @@ test('video-player: if exist a reference snapshot should match with it', async f
 
   function compareOrTakeSnapshot(): void {
     snapshotId += 1
-    void assertSnapshot(
+    assertSnapshot(
       `screenshot/$explorer_snapshot_video_player_${snapshotId}.png`,
       Vector3.create(8, 8, 8),
       Vector3.create(8, 8, 16)
-    )
-    successed++
+    ).then(() => {
+      successed++
+    }).catch(console.error)
   }
 
+  let lastTimestampState = -1
   function snapshotSystem(dt: number): void {
+    const videoState = videoEventsSystem.getVideoState(screenEntity)
+    if (videoState?.timestamp === undefined) return
+    if (videoState?.timestamp === lastTimestampState) {
+      return
+    }
+
+    lastTimestampState = videoState?.timestamp
+    console.log ({state: videoEventsSystem.getVideoState(screenEntity)})
+
     timer -= dt
     if (timer <= 0) {
       timer = 1
@@ -68,6 +80,7 @@ test('video-player: if exist a reference snapshot should match with it', async f
     if (snapshotsQuantity === snapshotId) {
       testFinished = true
     }
+
   }
 
   engine.addSystem(snapshotSystem)
