884b1b1dbb3a4d0035ff32c93678ed8fed1662d0
Seba Di Lauro <jsdilauro@gmail.com>
2023-12-27 12:10:24 -0300

    modify according comments

--- 54,-66-ui-dropdown/src/tests/ui-dropdown/index.test.tsx
+++ 54,-66-ui-dropdown/src/tests/ui-dropdown/index.test.tsx
@@ -1,4 +1,4 @@
-import { test } from 'testing-library/src/testing'
+import { skipTest, test } from 'testing-library/src/testing'
 import { CustomReactEcsRenderer } from 'testing-library/src/utils/ui'
 
 import { Color4, Vector3 } from '@dcl/sdk/math'
@@ -6,33 +6,33 @@ import ReactEcs, { Dropdown, Label, UiEntity } from '@dcl/sdk/react-ecs'
 import { customAddEntity } from 'testing-library/src/utils/entity'
 import { assertSnapshot } from 'testing-library/src/utils/snapshot-test'
 
-// let clicked: boolean = false
+let clicked: boolean = false
 
-// function ChangeColor(index: number): void {
-//   console.log('dropdown clicked')
-//   let color: Color4 = Color4.Black()
-//   for (const [entity] of engine.getEntitiesWith(Material)) {
-//     switch (index) {
-//       case 0:
-//         color = Color4.Black()
-//         clicked = false
-//         break
-//       case 1:
-//         color = Color4.Red()
-//         clicked = true
-//         break
-//       case 2:
-//         color = Color4.Blue()
-//         clicked = true
-//         break
-//       case 3:
-//         color = Color4.Green()
-//         clicked = true
-//         break
-//     }
-//     Material.setPbrMaterial(entity, { albedoColor: color })
-//   }
-// }
+function ChangeColor(index: number): void {
+  console.log('dropdown clicked')
+  let color: Color4 = Color4.Black()
+  for (const [entity] of engine.getEntitiesWith(Material)) {
+    switch (index) {
+      case 0:
+        color = Color4.Black()
+        clicked = false
+        break
+      case 1:
+        color = Color4.Red()
+        clicked = true
+        break
+      case 2:
+        color = Color4.Blue()
+        clicked = true
+        break
+      case 3:
+        color = Color4.Green()
+        clicked = true
+        break
+    }
+    Material.setPbrMaterial(entity, { albedoColor: color })
+  }
+}
 
 function TestElementDropdown(): ReactEcs.JSX.Element {
   return (
@@ -64,7 +64,7 @@ function TestElementDropdown(): ReactEcs.JSX.Element {
         fontSize={18}
         color={Color4.White()}
         options={[`Black`, `Red`, `Blue`, `Green`]}
-        // onChange={ChangeColor}
+        onChange={ChangeColor}
         uiTransform={{
           width: '100px',
           height: '60'
@@ -88,72 +88,81 @@ test('ui-dropdown: this test only check the visual style of dropdown', async fun
   )
 })
 
-// test('ui-dropdown: should change background color to red', async function (context) {
-//   customAddEntity.clean()
-//   clicked = false
-//   CustomReactEcsRenderer.destroy()
-//   CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
-//   await context.helpers.waitTicksUntil(() => {
-//     if (clicked) {
-//       return true
-//     } else {
-//       return false
-//     }
-//   })
+skipTest(
+  'ui-dropdown: should change background color to red',
+  async function (context) {
+    customAddEntity.clean()
+    clicked = false
+    CustomReactEcsRenderer.destroy()
+    CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
+    await context.helpers.waitTicksUntil(() => {
+      if (clicked) {
+        return true
+      } else {
+        return false
+      }
+    })
 
-//   await context.helpers.waitNTicks(5)
+    await context.helpers.waitNTicks(5)
 
-//   await assertSnapshot(
-//     'screenshot/$explorer_snapshot_ui_dropdown_red.png',
-//     Vector3.create(8, 1, 10),
-//     Vector3.create(8, 1, 8)
-//   )
+    await assertSnapshot(
+      'screenshot/$explorer_snapshot_ui_dropdown_red.png',
+      Vector3.create(8, 1, 10),
+      Vector3.create(8, 1, 8)
+    )
 
-//   ChangeColor(0)
-// })
+    ChangeColor(0)
+  }
+)
 
-// test('ui-dropdown: should change background color to blue', async function (context) {
-//   clicked = false
-//   CustomReactEcsRenderer.destroy()
-//   CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
-//   await context.helpers.waitTicksUntil(() => {
-//     if (clicked) {
-//       return true
-//     } else {
-//       return false
-//     }
-//   })
+skipTest(
+  'ui-dropdown: should change background color to blue',
+  async function (context) {
+    clicked = false
+    CustomReactEcsRenderer.destroy()
+    CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
+    await context.helpers.waitTicksUntil(() => {
+      if (clicked) {
+        return true
+      } else {
+        return false
+      }
+    })
 
-//   await context.helpers.waitNTicks(5)
+    await context.helpers.waitNTicks(5)
 
-//   await assertSnapshot(
-//     'screenshot/$explorer_snapshot_ui_dropdown_blue.png',
-//     Vector3.create(8, 1, 10),
-//     Vector3.create(8, 1, 8)
-//   )
+    await assertSnapshot(
+      'screenshot/$explorer_snapshot_ui_dropdown_blue.png',
+      Vector3.create(8, 1, 10),
+      Vector3.create(8, 1, 8)
+    )
 
-//   ChangeColor(0)
-// })
+    ChangeColor(0)
+  }
+)
 
-// test('ui-dropdown: should change background color to green', async function (context) {
-//   clicked = false
-//   CustomReactEcsRenderer.destroy()
-//   CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
-//   await context.helpers.waitTicksUntil(() => {
-//     if (clicked) {
-//       return true
-//     } else {
-//       return false
-//     }
-//   })
+skipTest(
+  'ui-dropdown: should change background color to green',
+  async function (context) {
+    clicked = false
+    CustomReactEcsRenderer.destroy()
+    CustomReactEcsRenderer.setUiRenderer(TestElementDropdown)
+    await context.helpers.waitTicksUntil(() => {
+      if (clicked) {
+        return true
+      } else {
+        return false
+      }
+    })
 
-//   await context.helpers.waitNTicks(5)
+    await context.helpers.waitNTicks(5)
 
-//   await assertSnapshot(
-//     'screenshot/$explorer_snapshot_ui_dropdown_green.png',
-//     Vector3.create(8, 1, 10),
-//     Vector3.create(8, 1, 8)
-//   )
+    await assertSnapshot(
+      'screenshot/$explorer_snapshot_ui_dropdown_green.png',
+      Vector3.create(8, 1, 10),
+      Vector3.create(8, 1, 8)
+    )
 
-//   ChangeColor(0)
-// })
+    ChangeColor(0)
+  }
+)
