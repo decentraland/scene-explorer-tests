49be6cfe1557b323932d70aa23ef862784c323da
Seba Di Lauro <jsdilauro@gmail.com>
2023-12-27 10:05:45 -0300

    ui-button is only visual test now

--- 54,-64-ui-button/src/tests/ui-button/index.test.tsx
+++ 54,-64-ui-button/src/tests/ui-button/index.test.tsx
@@ -1,19 +1,18 @@
 import { test } from 'testing-library/src/testing'
 import { CustomReactEcsRenderer } from 'testing-library/src/utils/ui'
 
-import { Material, engine } from '@dcl/sdk/ecs'
-import { Color4, Vector3 } from '@dcl/sdk/math'
+import { Vector3 } from '@dcl/sdk/math'
 import ReactEcs, { Button, UiEntity } from '@dcl/sdk/react-ecs'
 import { assertSnapshot } from 'testing-library/src/utils/snapshot-test'
 import { getScreenCanvasInfo } from 'testing-library/src/utils/ui/ui-utils'
 
-let clicked: boolean = false
+//let clicked: boolean = false
 
-function ChangeColor(color: Color4): void {
-  for (const [entity] of engine.getEntitiesWith(Material)) {
-    Material.setPbrMaterial(entity, { albedoColor: color })
-  }
-}
+// function ChangeColor(color: Color4): void {
+//   for (const [entity] of engine.getEntitiesWith(Material)) {
+//     Material.setPbrMaterial(entity, { albedoColor: color })
+//   }
+// }
 
 function TestElementButton(): ReactEcs.JSX.Element {
   const screenSize = getScreenCanvasInfo()
@@ -27,26 +26,41 @@ function TestElementButton(): ReactEcs.JSX.Element {
       <Button
         value="Click change backgound to blue"
         variant="primary"
-        uiTransform={{ width: 200, height: 50 }}
+        uiTransform={{ width: 200, height: 50, margin:{left:156, top:231},  }}
         onMouseDown={() => {
-          clicked = true
-          ChangeColor(Color4.Blue())
+          // clicked = true
+          // ChangeColor(Color4.Blue())
         }}
       />
     </UiEntity>
   )
 }
 
-test('ui-button: should change color to blue', async function (context) {
+// test('ui-button: should change color to blue', async function (context) {
+//   CustomReactEcsRenderer.destroy()
+//   CustomReactEcsRenderer.setUiRenderer(TestElementButton)
+//   await context.helpers.waitTicksUntil(() => {
+//     if (clicked) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   })
+
+//   await context.helpers.waitNTicks(5)
+
+//   await assertSnapshot(
+//     'screenshot/$explorer_snapshot_ui_button.png',
+//     Vector3.create(8, 1, 10),
+//     Vector3.create(8, 1, 8)
+//   )
+
+//   ChangeColor(Color4.Black())
+// })
+
+test('ui-button: this test only check the visual style of button', async function (context) {
   CustomReactEcsRenderer.destroy()
   CustomReactEcsRenderer.setUiRenderer(TestElementButton)
-  await context.helpers.waitTicksUntil(() => {
-    if (clicked) {
-      return true
-    } else {
-      return false
-    }
-  })
 
   await context.helpers.waitNTicks(5)
 
@@ -55,6 +69,4 @@ test('ui-button: should change color to blue', async function (context) {
     Vector3.create(8, 1, 10),
     Vector3.create(8, 1, 8)
   )
-
-  ChangeColor(Color4.Black())
 })
